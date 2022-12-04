fb5c420d857d0e48b0768ec8d5381b827dc4a954
Darkunitx <darkunitx@live.com>
2022-12-03 20:54:08 -0600

    added jquery

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,37 @@
+$(document).ready(function() {
+
+
+var apikey = "2329612b90eeda34110cadd2c06ae7c5";
+var searchFormEl = document.querySelector('#search-input');
+
+
+function handleSearchFormSubmit() {
+  
+    var searchInputVal = document.querySelector('#search-input').value;
+    console.log(searchInputVal);
+
+
+}
+
+handleSearchFormSubmit();
+
+
+
 function weatherChecker () {
 
-    var apikey = "2329612b90eeda34110cadd2c06ae7c5";
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q="
+    fetch("https://api.openweathermap.org/data/2.5/forecast?"
+    + "lat=44.7"
+    + "&lon=56.576"
+     + "&limit=5"
      + "&appid="
-      + apikey )
+     + apikey )
       .then((response) => response.json())
       .then((data) => console.log(data));
 }
+
 weatherChecker();
+
+searchFormEl.addEventListener('click', handleSearchFormSubmit);
+
+});
