8074fc95d62f8baf294633a113ff517bef9d46df
Darkunitx <darkunitx@live.com>
2022-12-06 13:59:18 -0600

    created functions that call on both apis

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,39 @@
-$(document).ready(function() {
-
-
 var apikey = "2329612b90eeda34110cadd2c06ae7c5";
 var searchFormEl = document.querySelector('#search-input');
 
 
 function handleSearchFormSubmit() {
-  
+
     var searchInputVal = document.querySelector('#search-input').value;
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="
+        + searchInputVal
+        + "&appid="
+        + apikey)
+        .then((response) => response.json())
+        .then((data) => {
+
+            var cityLat = data[0].lat
+            var cityLon = data[0].lon
+            console.log(cityLat);
+            console.log(cityLon);
+            console.log(data)
+
+            weatherChecker(cityLat, cityLon);
+        });
+
+
     console.log(searchInputVal);
 
 
 }
 
-handleSearchFormSubmit();
+
+function weatherChecker(lat, lon) {
 
 
-
-function weatherChecker () {
-
-
-    fetch("https://api.openweathermap.org/data/2.5/forecast?"
-    + "lat=44.7"
-    + "&lon=56.576"
-     + "&limit=5"
-     + "&appid="
-     + apikey )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}`)
+        .then((response) => response.json())
+        .then((data) => console.log("secondApiCall", data));
 }
 
-weatherChecker();
-
 searchFormEl.addEventListener('click', handleSearchFormSubmit);
-
-});
